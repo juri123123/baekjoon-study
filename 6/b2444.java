796231e8a03311c6e0511a77d03b3efaaf8e2a33
JURI <jr4426@naver.com>
2023-07-15 23:59:30 +0900

    commit

--- 6/b2444.java
+++ 6/b2444.java
@@ -1,38 +1,38 @@
-        import java.io.*;
-        import java.util.*;
+        // import java.io.*;
+        // import java.util.*;
 
-        public class b2444{
-            public static void main(String[] args) throws IOException {
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // public class b2444{
+        //     public static void main(String[] args) throws IOException {
+        //         BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        //         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-                int n = Integer.parseInt(br.readLine());
-                for(int i = 1; i < n; i++) {
-                    for(int j = n-1; j >= i; j--) {
-                        bw.write(".");
-                    }
-                    for (int j = 0; j < 2*i-1; j++) {
-                        bw.write("*");
-                    }
+        //         int n = Integer.parseInt(br.readLine());
+        //         for(int i = 1; i < n; i++) {
+        //             for(int j = n-1; j >= i; j--) {
+        //                 bw.write(".");
+        //             }
+        //             for (int j = 0; j < 2*i-1; j++) {
+        //                 bw.write("*");
+        //             }
                  
-                    bw.write("\n");
-                }
-                for(int i=0; i< 2*n-1; i++) {
-                    bw.write("*");
-                }
-                bw.write("\n");
-                for(int i = n-1; i > 0; i--) {
-                    for(int j = 0; j < n-i; j++) {
-                        bw.write(".");
-                    }
-                    for (int j = 0; j < 2*i-1; j++) {
-                        bw.write("*");
-                    }
+        //             bw.write("\n");
+        //         }
+        //         for(int i=0; i< 2*n-1; i++) {
+        //             bw.write("*");
+        //         }
+        //         bw.write("\n");
+        //         for(int i = n-1; i > 0; i--) {
+        //             for(int j = 0; j < n-i; j++) {
+        //                 bw.write(".");
+        //             }
+        //             for (int j = 0; j < 2*i-1; j++) {
+        //                 bw.write("*");
+        //             }
                
-                    bw.write("\n");
-                }
-                br.close();
-                bw.flush();
+        //             bw.write("\n");
+        //         }
+        //         br.close();
+        //         bw.flush();
 
-            }
-        }
+        //     }
+        // }
