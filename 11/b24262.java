85e3eda4b525c085314a178807c5a179714ebe7f
JURI <jr4426@naver.com>
2023-07-21 01:49:48 +0900

    11-1

---
+++ 11/b24262.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class b24262{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(1);
+        System.out.println(0);
+        br.close();
+        bw.flush();
+
+    }
+}
