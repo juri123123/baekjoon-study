74021daa5cf37fa7d11d1a0821d8971e2aa72a03
JURI <jr4426@naver.com>
2023-11-03 09:37:43 +0900

    [BOJ] N과M/35m

---
+++ Algo/BackTracking/b15649.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class b15649{
+    static int n;
+    static int m;
+    static int[] arr;
+    static boolean[] flag;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input);
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        flag = new boolean[n];
+
+        tracking(0);
+
+        br.close();
+        bw.flush();
+
+    }
+
+    static void tracking(int idx) {
+        if(idx == m) {
+            for(int i=0; i<m; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+
+        }
+
+        for(int i=0; i<n; i++) {
+            if(!flag[i]) {
+                flag[i] = true;
+                arr[idx] = i+1;
+                tracking(idx + 1);
+
+                flag[i] = false;
+            }
+        }
+    }
+
+
+}
