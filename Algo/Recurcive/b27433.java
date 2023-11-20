9c889ee9e93a7990d405649d00d47e543c8b9c3f
JURI <jr4426@naver.com>
2023-11-20 09:18:50 +0900

    [BOJ] 팩토리얼/10m

---
+++ Algo/Recurcive/b27433.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class b27433{
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        bw.write(factorial(n) + "\n");
+
+        br.close();
+        bw.flush();
+
+    }
+
+    static int factorial(int x) {
+        
+        if (x==0) {
+            return 1;
+        } else if (x==1) {
+            return 1;
+        }
+
+        return  x * factorial(x-1);
+    }
+}
